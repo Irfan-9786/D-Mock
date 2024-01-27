cb9dfd9db331e3672d9166adca7cc7151672ed68
Irfan Shaikh <Irfan@123.gmail>
2024-01-27 13:42:01 +0530

    addition of two numbers code

---
+++ Addition/src/com/velocity/AdditionProgram.java
@@ -0,0 +1,11 @@
+package com.velocity;
+
+public class AdditionProgram {
+
+	public static void main(String[] args) {
+		int num1=5;
+		int num2=10;
+		int sum=num1+num2;
+		System.out.println("Addition is "+sum);
+	}
+}
